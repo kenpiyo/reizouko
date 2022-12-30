0db87774b4ada7c0165b77f0b3954334cb21ad13
kenpiyo <dddoublelari@gmail.com>
2022-12-31 06:10:03 +0900

    first commit

---
+++ src/main/java/com/kenpiyo/reizouko/ReizoukoApplication.java
@@ -0,0 +1,13 @@
+package com.kenpiyo.reizouko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReizoukoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReizoukoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kenpiyo/reizouko/ReizoukoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenpiyo.reizouko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReizoukoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
