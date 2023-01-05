896bfb619f0adcd435b08933d26da4ab3da18706
kenpiyo <dddoublelari@gmail.com>
2023-01-05 10:15:04 +0900

    食材を登録する画面を作成

--- src/main/java/com/kenpiyo/reizouko/Controller/FridgeController.java
+++ src/main/java/com/kenpiyo/reizouko/Controller/FridgeController.java
@@ -14,9 +14,16 @@ public class FridgeController {
     
     private final FridgeService fridgeService;
 
+    // 冷蔵庫に登録されている食材一覧を表示
     @GetMapping("/contents")
     public String fridgeContents(Model model){
         model.addAttribute("contentsList", fridgeService.findContentsAll());
         return "contentsList";
+    }
+
+    // 冷蔵庫に食材を登録する画面を表示
+    @GetMapping("/resister")
+    public String showResisterForm(){
+        return "syokuzaiResister";
     }
 }
